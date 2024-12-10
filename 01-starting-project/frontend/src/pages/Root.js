1638fed53ad4d2caff8b51f272d8f553f5c8ad28
serhiisaveliev <saveliev.sergeyne@gmail.com>
2024-12-10 14:15:23 +0100

    Add files via upload
    first commit

---
+++ 01-starting-project/frontend/src/pages/Root.js
@@ -0,0 +1,47 @@
+import {
+  Outlet,
+  // useNavigation,
+  useRouteLoaderData,
+  useSubmit,
+} from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
+
+function RootLayout() {
+  const token = useRouteLoaderData("root");
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  // const navigation = useNavigation();
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
