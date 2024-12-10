b1fa438909a6ec6e1b0a46ac4bb0ba9dbad67f6c
serhiisaveliev <saveliev.sergeyne@gmail.com>
2024-12-10 14:27:38 +0100

    Add files via upload
    first commit

---
+++ react-ts/src/store/todos-context.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode, useState } from "react";
+import Todo from "../models/todo";
+
+type TTodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+export const TodosContext = React.createContext<TTodosContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC<{ children: ReactNode }> = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
+  };
+
+  const contextValue: TTodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
